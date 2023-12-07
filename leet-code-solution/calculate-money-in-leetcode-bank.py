81043482884006f8d5113a02db02d72d226e5ebc
urjilaa <138873507+urjilaa@users.noreply.github.com>
2023-12-06 16:32:14 -0800

    Added solution for Calculate Money in Leetcode Bank

---
+++ leet-code-solution/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        ans=0
+        monday=1
+
+        while n > 0:
+            for day in range(min(n, 7)):
+                
+                ans += monday + day
+                print(ans)
+
+            n -= 7
+            monday += 1
+
+        return ans
+       
+       
