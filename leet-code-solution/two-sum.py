715bc7aad1a02ddf9117d8a9ed64f69eaeeb7952
urjilaa <138873507+urjilaa@users.noreply.github.com>
2023-12-11 09:12:09 -0800

    Added solution for Two Sum

---
+++ leet-code-solution/two-sum.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def twoSum(self, nums, target):
+        nums_hash = {}
+        for i in range(len(nums)):
+            if target - nums[i] in nums_hash: 
+                return [nums_hash[target - nums[i]], i]
+            nums_hash[nums[i]] = i
