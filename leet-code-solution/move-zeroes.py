f30ae63561615b619908749a3024f09ad2b148be
urjilaa <138873507+urjilaa@users.noreply.github.com>
2024-01-02 08:14:16 -0800

    Added solution for Move Zeroes

---
+++ leet-code-solution/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        index=0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[index], nums[i]= nums[i], nums[index]
+                index +=1
