560097b91a2a9326d766299ed21b4f1dfb64055b
urjilaa <138873507+urjilaa@users.noreply.github.com>
2024-02-15 03:41:19 -0800

    Add solution for Palindrome Linked List

--- leetcode/leetcode/palindrome-linked-list.py
+++ leetcode/leetcode/palindrome-linked-list.py
@@ -5,10 +5,25 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        nums = []
+        #finding the middle of the list
+        slow = fast = head 
+        while fast and fast.next: 
+            fast = fast.next.next 
+            slow = slow.next 
 
-        while head:
-            nums.append(head.val)
-            head = head.next
-            
-        return nums == nums[::-1] 
+        #reversing the second half
+        prev = None 
+        while slow: 
+            nxt = slow.next 
+            slow.next = prev 
+            prev = slow
+            slow = nxt 
+
+        #check palindrom
+        left, right = head, prev 
+        while right: 
+            if left.val != right.val: 
+                return False 
+            left = left.next 
+            right = right.next 
+        return True 
