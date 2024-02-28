3a4bdccbfe554794dc1146f88bac3033bfd3e4a5
urjilaa <138873507+urjilaa@users.noreply.github.com>
2024-02-28 10:13:15 -0800

    Add solution for Count Good Numbers

---
+++ leetcode/leetcode/count-good-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    store = {}
+    def countGoodNumbers(self, n: int) -> int:
+        if n == 2:
+            return 20
+        elif n == 1:
+            return 5
+        if n not in self.store:
+            if n%2 == 0:
+                self.store[n] = (self.countGoodNumbers(n-1) * 4)%(10**9 + 7)
+            else:
+                self.store[n] = (self.countGoodNumbers(n//2) * self.countGoodNumbers((n//2)+1))%(10**9 + 7)
+        
+        return (self.store[n])%(10**9 + 7)
+        
